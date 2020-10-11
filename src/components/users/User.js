d65064583c056178074966257dd6f332d3836cd1
Chi Mei Wang <kimmy0929@gmail.com>
2020-10-11 20:47:08 +0900

    created user page

---
+++ src/components/users/User.js
@@ -0,0 +1,106 @@
+//rce
+import React, { Component, Fragment } from 'react';
+import { Link } from 'react-router-dom';
+import Spinner from '../layout/Spinner';
+import PropTypes from 'prop-types';
+
+class User extends Component {
+  componentDidMount() {
+    this.props.getUser(this.props.match.params.login);
+  }
+
+  static propTypes = {
+    user: PropTypes.object.isRequired,
+    loading: PropTypes.bool.isRequired,
+    getUser: PropTypes.func.isRequired
+  };
+
+  render() {
+    const { user, loading } = this.props;
+
+    if (loading) {
+      return <Spinner />;
+    } else {
+      return (
+        <Fragment>
+          <Link to='/' className='btn btn-light'>
+            Back
+          </Link>
+          Hireable:{' '}
+          {user.hireable ? (
+            <i className='fas fa-check text-success'></i>
+          ) : (
+            <i className='fas fa-time-circle text-danger'></i>
+          )}
+          <div className='card grid-2'>
+            <div className='all-center'>
+              <img
+                src={user.avatar_url}
+                alt={user.login}
+                className='round-img'
+                style={{ width: '150px' }}
+              />
+              <h1>{user.name}</h1>
+              <p>Location: {user.location}</p>
+            </div>
+            <div>
+              {user.bio && (
+                <Fragment>
+                  <h3>Bio</h3>
+                  <p>{user.bio}</p>
+                </Fragment>
+              )}
+              <a href={user.html_url} className='btn btn-dark my-1'>
+                View Profile on Github
+              </a>
+              <ul>
+                <li>
+                  {user.login && (
+                    <Fragment>
+                      <strong>Username: {user.login}</strong>
+                    </Fragment>
+                  )}
+                </li>
+                <li>
+                  {user.company && (
+                    <Fragment>
+                      <strong>Company: {user.company}</strong>
+                    </Fragment>
+                  )}
+                </li>
+                <li>
+                  {user.blog && (
+                    <Fragment>
+                      <strong>
+                        Website:{' '}
+                        <a target='_blank' href={user.blog}>
+                          {user.blog}
+                        </a>
+                      </strong>
+                    </Fragment>
+                  )}
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className='card text-center'>
+            <div className='badge badge-primary'>
+              Followers: {user.followers}
+            </div>
+            <div className='badge badge-success'>
+              Following: {user.following}
+            </div>
+            <div className='badge badge-danger'>
+              Public Repos: {user.public_repos}
+            </div>
+            <div className='badge badge-dark'>
+              Public Gists: {user.public_gists}
+            </div>
+          </div>
+        </Fragment>
+      );
+    }
+  }
+}
+
+export default User;
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 // Class component
@@ -23,9 +24,9 @@ const UserItem = ({ user: { id, login, avatar_url, html_url } }) => {
         style={{ width: '100px' }}
       />
       <h3>{login}</h3>
-      <a href={html_url} className='btn btn-dark btn-sm my-1'>
+      <Link to={`/user/${login}`} className='btn btn-dark btn-sm my-1'>
         More
-      </a>
+      </Link>
     </div>
   );
 };
