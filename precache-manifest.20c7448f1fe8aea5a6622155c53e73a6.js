f0404e795cafb6b1056191ce6dbf653120b23987
Chi Mei Wang <kimmy0929@gmail.com>
2020-10-17 13:17:18 +0900

    Updates

---
+++ precache-manifest.20c7448f1fe8aea5a6622155c53e73a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a68f54381acd788711a6ed63e7adc37a",
+    "url": "/contribstats/index.html"
+  },
+  {
+    "revision": "d8cf734686a9ead734c3",
+    "url": "/contribstats/static/css/main.86af76df.chunk.css"
+  },
+  {
+    "revision": "7ae88820725b7a5173e4",
+    "url": "/contribstats/static/js/2.1f393c97.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/contribstats/static/js/2.1f393c97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8cf734686a9ead734c3",
+    "url": "/contribstats/static/js/main.4e84e62a.chunk.js"
+  },
+  {
+    "revision": "2eef8be7ecef5f13e943",
+    "url": "/contribstats/static/js/runtime-main.2ee6da48.js"
+  }
+]);
