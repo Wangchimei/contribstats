c6042045679fbc565e9e0dd544c806d858560147
Chi Mei Wang <kimmy0929@gmail.com>
2020-10-17 14:35:25 +0900

    Updates

---
+++ precache-manifest.8eea76f20c8b9b44e0f37b7cf3b7456e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01ce9122685e77a8533abbfcc6a94d40",
+    "url": "http://wangchimei.github.io/contribstats/index.html"
+  },
+  {
+    "revision": "758a55ad9937472f1bd3",
+    "url": "http://wangchimei.github.io/contribstats/static/css/main.86af76df.chunk.css"
+  },
+  {
+    "revision": "7ae88820725b7a5173e4",
+    "url": "http://wangchimei.github.io/contribstats/static/js/2.1f393c97.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "http://wangchimei.github.io/contribstats/static/js/2.1f393c97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "758a55ad9937472f1bd3",
+    "url": "http://wangchimei.github.io/contribstats/static/js/main.ec2d0cf7.chunk.js"
+  },
+  {
+    "revision": "870ba692be0f95a9c3ab",
+    "url": "http://wangchimei.github.io/contribstats/static/js/runtime-main.8faf2837.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "http://localhost:3000/precache-manifest.00e4a2f2ca25da0a8797ef94be09ef41.js"
+  "http://wangchimei.github.io/contribstats/precache-manifest.8eea76f20c8b9b44e0f37b7cf3b7456e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("http://localhost:3000/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("http://wangchimei.github.io/contribstats/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
