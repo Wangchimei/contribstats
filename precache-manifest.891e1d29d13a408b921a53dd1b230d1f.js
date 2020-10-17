5a9f2e11bacbde6f7d3a44acbfad5e51b410e468
Chi Mei Wang <kimmy0929@gmail.com>
2020-10-17 14:52:04 +0900

    Updates

---
+++ precache-manifest.891e1d29d13a408b921a53dd1b230d1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ec6bfeaf5888ab6b72315560dd5f958",
+    "url": "https://wangchimei.github.io/contribstats/index.html"
+  },
+  {
+    "revision": "448aeff316d3e039fab8",
+    "url": "https://wangchimei.github.io/contribstats/static/css/main.86af76df.chunk.css"
+  },
+  {
+    "revision": "7ae88820725b7a5173e4",
+    "url": "https://wangchimei.github.io/contribstats/static/js/2.1f393c97.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "https://wangchimei.github.io/contribstats/static/js/2.1f393c97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "448aeff316d3e039fab8",
+    "url": "https://wangchimei.github.io/contribstats/static/js/main.1b42c6ba.chunk.js"
+  },
+  {
+    "revision": "dc92907c4bde275fdd8f",
+    "url": "https://wangchimei.github.io/contribstats/static/js/runtime-main.0dab6c0f.js"
+  }
+]);
