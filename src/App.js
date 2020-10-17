199e6a3018de42d9408eee8531a54d2e03e31f6e
Chi Mei Wang <kimmy0929@gmail.com>
2020-10-17 13:22:17 +0900

    modified route

--- src/App.js
+++ src/App.js
@@ -81,7 +81,7 @@ class App extends Component {
             <Switch>
               <Route
                 exact
-                path='/'
+                path='/home'
                 render={(props) => (
                   <Fragment>
                     <Search
